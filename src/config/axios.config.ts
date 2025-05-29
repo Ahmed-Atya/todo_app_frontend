2159b9fa82f64dc917a800695fa56ff376d523eb
Ahmed-Atya <ahmedatya@outlook.com>
2025-05-30 00:23:08 +0300

    Fixing axios limit

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://blessed-badge-e2ca161c14.strapiapp.com//api",
-  timeout: 7000,
+  timeout: 20000,
 });
 
 export default axiosInstance;
