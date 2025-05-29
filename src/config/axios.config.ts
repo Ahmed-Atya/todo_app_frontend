82834d6bd1bfec92b561eb97ed215b87781aaff6
Ahmed-Atya <ahmedatya@outlook.com>
2025-05-30 00:19:05 +0300

    final

---
+++ src/config/axios.config.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://blessed-badge-e2ca161c14.strapiapp.com//api",
+  timeout: 7000,
+});
+
+export default axiosInstance;
